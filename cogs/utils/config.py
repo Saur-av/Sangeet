8d3aa0ce420d016578da311ff358af230cb06531
Saurav <saurav.poudll@gmail.com>
2024-02-13 00:49:39 +0545

    Sample config.py

---
+++ cogs/utils/config.py
@@ -0,0 +1,6 @@
+token = "DiscordBotToken"
+db_host = "PostgresDatabaseHost"
+db_name = "PostgresDatabaseName"
+db_port = 0
+db_user = "PostgresDatabaseUser"
+db_password = "PostgresDatabasePassword"
